cc78240660203f8f91675e5048b8e03d9ae40eb3
凤之兮原 <1577310596@qq.com>
2023-01-27 22:05:20 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <main font-sans text="gray-700 dark:gray-200">
+    <RouterView />
+    <TheFooter />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>123123</h1>
+</template>
+
+<script setup lang='ts'>
+  
+</script>
+
+<style scoped>
+  
+</style>
