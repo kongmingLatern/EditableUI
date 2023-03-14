5171bf188fc5ee82030a97cf45751d36105597ad
凤之兮原 <1577310596@qq.com>
2023-03-14 22:09:13 +0800

    feat: support vue component props

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@ import Edit from '~/components/Edit'
 </script>
 <template>
   <button btn>I'm common button</button>
+  <img src="./assets/cj1.jpeg" alt="this a picture" />
   <Edit>
-    <Index src="~/assets/cj1.jpeg" alt="this a picture" />
     <button>I'm inside Edit button</button>
     <div>
       12313
       <span>12313</span>
     </div>
+    <B />
   </Edit>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   src: string
   alt: string
 }>()
